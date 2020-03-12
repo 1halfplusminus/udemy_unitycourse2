01832ae23694e0181a063ac6d55d5698078a3898
dartuchiwa <dartuchiwa@gmail.com>
2020-03-12 20:12:29 +1100

    Feat - refractoring

---
+++ Terminal Hacker/Assets/Data Component/DirectoryPasswordElement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+[InternalBufferCapacity(100)]
+public struct DirectoryPasswordElement : IBufferElementData
+{
+    public Entity Value;
+}
